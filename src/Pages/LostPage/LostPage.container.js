f86700409fa6c9de9c3a69759ffaee9a2052e122
Florentin <florentin.coyard@gmail.com>
2022-06-14 10:41:46 +0200

    feat(major): added router and /lost page - added react-router-dom - added LostPage for /lost route

---
+++ src/Pages/LostPage/LostPage.container.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from "react";
+
+import { Navigate } from "react-router-dom";
+
+import "./LostPage.scss";
+
+/**
+ *
+ * @returns {React.JSXElementConstructor}
+ */
+const LostPage = () => {
+  const [shouldRedirect, setShouldRedirect] = useState(false);
+
+  useEffect(() => {
+    // we wait for 5s before changing state that will help redirect
+    setTimeout(() => {
+      setShouldRedirect(true);
+    }, 5000);
+  }, []);
+
+  //redirect to HomePage after 5s
+  if (shouldRedirect) {
+    return <Navigate replace to="/" />;
+  }
+
+  return (
+    <div className="lostpage">
+      <img
+        alt="Confused Kermit being lost"
+        src={
+          "https://c.tenor.com/CbhnRg0n7ksAAAAC/kermit-the-frog-looking-for-directions.gif"
+        }
+      />
+    </div>
+  );
+};
+
+export default LostPage;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import HomePage from "./Pages/HomePage/HomePage.container";
+import LostPage from "./Pages/LostPage/LostPage.container";
 
 import "./reset.css"; // we reset all native css
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <HomePage />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/lost" element={<LostPage />} />
+        <Route exact path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
